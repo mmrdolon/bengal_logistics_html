b2594f125d048a89f75870e2e760261f5d9f34c0
Md. Mamdudur Rahman <mamdud@dcastalia.com>
2015-10-20 12:42:33 +0600

    Html of Bengal Logisitis...

---
+++ footer.php
@@ -0,0 +1,99 @@
+<!-----------------  Footer  ----------------->
+
+
+<div class="container-fluid footerMenu">
+    <div class="row">
+        <div class="socialIcons">
+            <a href="#"><p class="footerHeading">BENGAL<br/>LOGISTICS<br>
+                    LIMITED</p>
+            </a>
+
+
+        </div>
+
+        <div class="aboutUs">
+            <p class="footerHeading">ABOUT US</p>
+
+            <ul>
+                <li><a href="#">CSR Activities</a></li>
+                <li><a href="#">The Logo</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Our History</a></li>
+                <li><a href="#">Vision & Mission</a></li>
+                <li><a href="#">Background</a></li>
+
+            </ul>
+
+        </div>
+
+
+        <div class="management"><p class="footerHeading">MANAGEMENT</p>
+            <ul>
+                <li><a href="#">Board of Directors</a></li>
+                <li><a href="#">Executive Body</a></li>
+                <li><a href="#">Team Members</a></li>
+            </ul>
+        </div>
+        <div class="services"><p class="footerHeading">SERVICES</p>
+            <ul>
+                <li><a href="#">Getting Started</a></li>
+                <li><a href="#">Curriculum</a></li>
+                <li><a href="#">Fee & Charges</a></li>
+            </ul>
+        </div>
+        <div class="newsEvents"><p class="footerHeading">NEWS & EVENTS</p>
+
+            <ul>
+                <li><a href="#">Press Release</a></li>
+                <li><a href="#">Brochures</a></li>
+                <li><a href="#">Photo Gallery</a></li>
+                <li><a href="#">Forms & Downloads</a></li>
+            </ul>
+        </div>
+        <div class="others"><p class="footerHeading"></p>
+
+
+            <!--<input type="text" placeholder="Search Here" class="inputBox"><a href="#"><img  class="searchIcon" src="images/search_icon.png" height="30px" width="30px"></a>-->
+
+
+            <!----------------- Search Box ------------>
+            <div>
+                <form name="searchBox" id="searchBox" action="#" method="post"> <input type="text" placeholder="Search here..." name="query" id="query"/>
+                    <input type="submit" name="sub" id="sub" value=" "/></form>
+
+            </div>
+
+
+            <p class="socialPages">SOCIAL PAGES</p>
+
+            <a class="message" target="_blank" href="http://bengallogistics.blogspot.com/2013/10/bangladesh-at-glance-french-version.html"><span class="french"></span>
+
+            </a>
+
+            <a class="facebook" target="_blank" href="http://bengallogistics.blogspot.com/2013/10/bangladesh-at-glance-french-version.html"><span class="italian"></span>
+
+            </a>
+            <a class="twitter" target="_blank" href="http://www.bengaltours.net/"><span class="japanese"> </span>
+
+            </a>
+            <a class="linkedin" target="_blank" href="http://bengaltours.com/images/German.pdf"><span class="german"> </span>
+
+            </a>
+
+
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid footerCopyright">
+    <div class="row">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 copyRight">&copy; Web Work for Women 2015. All rights reserved. </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 designDeveloped"><a href="http://www.dcastalia.com/#/" target="_blank">Design and Development by Dcastalia</a></div>
+    </div>
+</div>
+
+
+<!--</div>-->
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bengal Logistics</title>
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="css/pure-drawer.css"/>
+
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <script src="http://code.jquery.com/jquery-1.8.2.min.js" type="text/javascript"></script>
+    <script src="js/jquery.carouFredSel-6.0.4-packed.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(function() {
+
+            $('#carousel ul').carouFredSel({
+                prev: '#prev',
+                next: '#next',
+                pagination: "#pager",
+                scroll: 1000
+            });
+
+        });
+    </script>
+
+
+</head>
+<body>
+
+
+<!---------------------  Top Header  -------------------->
+
+
+<div class="container-fluid topHeader">
+    <div class="row">
+
+
+        <div class="menuBanner">
+            <a href="#">
+                <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 logo">
+
+                </div></a>
+
+            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+
+            </div>
+
+
+            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 topBanner">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 flagIcon">
+                        <a class="frenchLInk" target="_blank" href="http://bengallogistics.blogspot.com/2013/10/bangladesh-at-glance-french-version.html"><span class="french"></span>
+
+                        </a>
+
+                        <a class="italianLInk" target="_blank" href="http://bengallogistics.blogspot.com/2013/10/bangladesh-at-glance-french-version.html"><span class="italian"></span>
+
+                        </a>
+                        <a class="japaneseLInk" target="_blank" href="http://www.bengaltours.net/"><span class="japanese"> </span>
+
+                        </a>
+                        <a class="germanLInk" target="_blank" href="http://bengaltours.com/images/German.pdf"><span class="german"> </span>
+
+                        </a>
+
+                    </div>
+                </div>
+
+
+
+
+
+                <div class="pure-container  visible-xs" data-effect="pure-effect-push" >
+                    <input type="checkbox" id="pure-toggle-right" class="pure-toggle" data-toggle="right"/>
+                    <label class="pure-toggle-label" for="pure-toggle-right" data-toggle-label="right"><span class="pure-toggle-icon"></span></label>
+
+                    <nav class="pure-drawer" data-position="right">
+                        <!--            <p style="padding: 100px 20px; margin: 0;">Insert your off-canvas content here</p>-->
+
+                        <!--<ul class="nav navbar-nav">-->
+
+
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li>
+                                <a href="#"> Company</a>
+
+                            </li>
+                            <li><a href="#">Bangladesh at a glance</a>
+                                <ul>
+                                    <li><a href="#">Explore Bangladesh</a></li>
+                                    <li><a href="#">World Heritage Tour</a></li>
+                                    <li><a href="#">Cultural Tour</a></li>
+                                    <li><a href="#">Cultural Tour</a></li>
+                                    <li><a href="#">Trip to tea capital</a></li>
+                                    <li>
+
+                                        <a href="#">Rocket Steamer</a>
+                                        <!--<ul>-->
+                                        <!--<li><a href="#">Trip to the Chittagong hill track</a></li>-->
+                                        <!--<li><a href="#">Responsible Tourism</a></li>-->
+                                        <!--<li><a href="#">Dhaka City Sight Seeing Suburbs</a></li>-->
+                                        <!--<li><a href="#">Hills & Beach</a></li>-->
+                                        <!--<li><a href="#">Tailor Made Tour</a></li>-->
+                                        <!--<li><a href="#">Photography Tour</a></li>-->
+                                        <!--<li><a href="#">Daylong Sundarbans</a></li>-->
+                                        <!--<li><a href="#">Trip to North Bengal</a></li>-->
+                                        <!--<li><a href="#">Village HomeStay</a></li>-->
+                                        <!--</ul>-->
+
+                                    <li><a href="#">Trip to the Chittagong hill track</a></li>
+                                    <li><a href="#">Responsible Tourism</a></li>
+                                    <li><a href="#">Dhaka City Sight Seeing Suburbs</a></li>
+                                    <li><a href="#">Hills & Beach</a></li>
+                                    <li><a href="#">Tailor Made Tour</a></li>
+                                    <li><a href="#">Photography Tour</a></li>
+                                    <li><a href="#">Daylong Sundarbans</a></li>
+                                    <li><a href="#">Trip to North Bengal</a></li>
+                                    <li><a href="#">Village HomeStay</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Interesting place</a></li>
+                            <li><a href="#">Contact us</a></li>
+
+                        </ul>
+
+                    </nav>
+
+                    <div class="pure-pusher-container">
+                        <div class="pure-pusher">
+                            <!--<p style="width: 300px; height: 1800px; margin: 100px auto;">-->
+
+                            <!--</p>-->
+
+                        </div>
+
+                    </div>
+                </div>
+
+
+
+
+
+
+
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 visible-sm visible-md visible-lg mainMenu">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li>
+                                <a href="#"> Company</a>
+
+                            </li>
+                            <li><a href="#">Bangladesh at a glance</a>
+                                <ul>
+                                    <li><a href="#">Explore Bangladesh</a></li>
+                                    <li><a href="#">World Heritage Tour</a></li>
+                                    <li><a href="#">Cultural Tour</a></li>
+                                    <li><a href="#">Cultural Tour</a></li>
+                                    <li><a href="#">Trip to tea capital</a></li>
+                                    <li>
+
+                                        <a href="#">Rocket Steamer</a>
+
+                                    <li><a href="#">Trip to the Chittagong hill track</a></li>
+                                    <li><a href="#">Responsible Tourism</a></li>
+                                    <li><a href="#">Dhaka City Sight Seeing Suburbs</a></li>
+                                    <li><a href="#">Hills & Beach</a></li>
+                                    <li><a href="#">Tailor Made Tour</a></li>
+                                    <li><a href="#">Photography Tour</a></li>
+                                    <li><a href="#">Daylong Sundarbans</a></li>
+                                    <li><a href="#">Trip to North Bengal</a></li>
+                                    <li><a href="#">Village HomeStay</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Interesting place</a></li>
+                            <li><a href="#">Contact us</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
